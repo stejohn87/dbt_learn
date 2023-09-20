44e32328ed397f0b36cfd956b2608b981c6b58d4
stejohn87 <88798863+stejohn87@users.noreply.github.com>
2023-09-20 00:23:08 -0400

    adding src tables

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,12 @@
+with raw_hosts as (
+    select * from AIRBNB.RAW.RAW_HOSTS
+)
+
+select 
+    id as host_id, 
+    NAME as host_name, 
+    is_superhost, 
+    created_at, 
+    updated_at
+from   
+    raw_hosts
